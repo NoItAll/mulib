160eeff5731811db563a18862402bdf5cb0ec86d
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2022-01-04 17:13:32 +0100

    Added test case for iinc

---
+++ src/test/java/de/wwu/mulib/transform_and_execute/examples/IincCheck.java
@@ -0,0 +1,18 @@
+package de.wwu.mulib.transform_and_execute.examples;
+
+import de.wwu.mulib.Mulib;
+import de.wwu.mulib.exceptions.MulibRuntimeException;
+
+public class IincCheck {
+
+    public static int exec() {
+        int i = Mulib.freeInt();
+        int j = i;
+        i++;
+        if (i <= j) {
+            throw new MulibRuntimeException("Must not occur.");
+        }
+        return 1;
+    }
+
+}
---
+++ src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/IincCheckExec.java
@@ -0,0 +1,34 @@
+package de.wwu.mulib.transform_and_execute.examples_executor;
+
+import de.wwu.mulib.MulibConfig;
+import de.wwu.mulib.TestUtility;
+import de.wwu.mulib.search.trees.ExceptionPathSolution;
+import de.wwu.mulib.search.trees.PathSolution;
+import de.wwu.mulib.transform_and_execute.examples.IincCheck;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IincCheckExec {
+
+    @Test
+    public void testIincCheck() {
+        List<List<PathSolution>> solutions = TestUtility.getAllSolutions((mb) -> _testIincCheck(mb), "exec");
+    }
+
+    private List<PathSolution> _testIincCheck(MulibConfig.MulibConfigBuilder mb) {
+        List<PathSolution> result = TestUtility.executeMulib(
+                "exec",
+                IincCheck.class,
+                1,
+                mb,
+                true
+        );
+        assertEquals(1, result.size());
+        assertTrue(result.stream().noneMatch(ps -> ps instanceof ExceptionPathSolution));
+        return result;
+    }
+}
