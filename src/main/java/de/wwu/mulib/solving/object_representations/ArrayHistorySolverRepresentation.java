3c17a83e58adae4c847e59a861fe749f12704cda
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2022-11-17 15:39:57 +0100

    Collapsing the indices into ranges does not seem to do much good, even for ordered machine-entries in PCAP example. This is the implementation used. It is commented out for now.

--- src/main/java/de/wwu/mulib/solving/object_representations/ArrayHistorySolverRepresentation.java
+++ src/main/java/de/wwu/mulib/solving/object_representations/ArrayHistorySolverRepresentation.java
@@ -7,11 +7,9 @@ import de.wwu.mulib.substitutions.PartnerClass;
 import de.wwu.mulib.substitutions.SubstitutedVar;
 import de.wwu.mulib.substitutions.primitives.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Contains the select for an array. A store yields a nested structure of ArrayHistorySolverRepresentations
@@ -26,7 +24,7 @@ public class ArrayHistorySolverRepresentation {
             ArrayAccessConstraint[] initialSelects,
             Class<?> valueType) {
         SubstitutedVar defaultValue;
-         if (Sbool.class.isAssignableFrom(valueType)) {
+        if (Sbool.class.isAssignableFrom(valueType)) {
             defaultValue = Sbool.ConcSbool.FALSE;
         } else if (Sbyte.class.isAssignableFrom(valueType)) {
             defaultValue = Sbyte.ConcSbyte.ZERO;
@@ -41,10 +39,12 @@ public class ArrayHistorySolverRepresentation {
         } else if (Sfloat.class.isAssignableFrom(valueType)) {
             defaultValue = Sfloat.ConcSfloat.ZERO;
         } else if (valueType.isArray() || PartnerClass.class.isAssignableFrom(valueType)) {
-             defaultValue = Sint.ConcSint.MINUS_ONE;
-         } else {
+            defaultValue = Sint.ConcSint.MINUS_ONE;
+        } else {
             throw new NotYetImplementedException();
         }
+        this.store = null;
+        this.beforeStore = null;
         this.defaultValue = defaultValue;
         this.selects = new ArrayList<>();
         for (ArrayAccessConstraint ac : initialSelects) {
@@ -55,10 +55,93 @@ public class ArrayHistorySolverRepresentation {
                     value
             ));
         }
-        this.store = null;
-        this.beforeStore = null;
+//        {
+//            final Map<Sprimitive, List<Sint>> sameValToIndices = new HashMap<>();
+//            Arrays.stream(initialSelects).sequential().forEach(s -> {
+//                List<Sint> indices = sameValToIndices.computeIfAbsent(s.getValue(), k -> new ArrayList<>());
+//                indices.add(s.getIndex());
+//            });
+//            final Map<Sprimitive, List<Sint[]>> sameValToRanges = new HashMap<>();
+//            sameValToIndices.forEach((key, value) -> {
+//                List<Sint[]> ranges = getRangesWithoutNull(value);
+//                sameValToRanges.put(key, ranges);
+//            });
+//
+//            for (Map.Entry<Sprimitive, List<Sint[]>> rangesForVal : sameValToRanges.entrySet()) {
+//                Sprimitive val = rangesForVal.getKey();
+//                for (Sint[] range : rangesForVal.getValue()) {
+//                    ArrayAccessSolverRepresentation aasr;
+//                    if (range.length == 1) {
+//                        aasr = new ArrayAccessSolverRepresentation(Sbool.ConcSbool.TRUE, range[0], val);
+//                    } else {
+//                        assert range.length == 2;
+//                        assert range[0] instanceof ConcSnumber && range[1] instanceof ConcSnumber;
+//                        ConcSnumber from = (ConcSnumber) range[0];
+//                        ConcSnumber to = (ConcSnumber) range[1];
+//                        aasr = new ArrayAccessSolverRepresentation(
+//                                Sbool.ConcSbool.TRUE,
+//                                (i) -> And.newInstance(Lte.newInstance(from, i), Lte.newInstance(i, to)),
+//                                val
+//                        );
+//                    }
+//                    selects.add(aasr);
+//                }
+//            }
+//        }
     }
 
+    public static List<Sint[]> getRangesWithoutNull(Collection<Sint> is) {
+        ConcSnumber[] concSnumbers = is.stream()
+                .filter(i -> i instanceof ConcSnumber && i != Sint.ConcSint.MINUS_ONE)
+                .sorted((i0, i1) -> {
+                    ConcSnumber c0 = (ConcSnumber) i0;
+                    ConcSnumber c1 = (ConcSnumber) i1;
+                    if (c0.intVal() < c1.intVal()) {
+                        return -1;
+                    } else if (c0.intVal() == c1.intVal()) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                }).toArray(ConcSnumber[]::new);
+        List<Sint[]> concs = new ArrayList<>();
+        ConcSnumber currentRangeStartingFrom = null;
+        for (int i = 0; i < concSnumbers.length; i++) {
+            if (i < concSnumbers.length-1) {
+                ConcSnumber i0 = concSnumbers[i];
+                ConcSnumber i1 = concSnumbers[i+1];
+                if (i0.intVal() + 1 == i1.intVal()) {
+                    if (currentRangeStartingFrom == null) {
+                        currentRangeStartingFrom = i0;
+                    }
+                } else {
+                    if (currentRangeStartingFrom == null) {
+                        concs.add(new Sint[] { (Sint) i0 });
+                    } else {
+                        concs.add(new Sint[] { (Sint) currentRangeStartingFrom, (Sint) i0 });
+                        currentRangeStartingFrom = null;
+                    }
+                }
+            } else {
+                if (currentRangeStartingFrom == null) {
+                    concs.add(new Sint[]{(Sint) concSnumbers[i]});
+                } else if (concSnumbers[i-1].intVal() + 1 == concSnumbers[i].intVal()) {
+                    concs.add(new Sint[] { (Sint) currentRangeStartingFrom, (Sint) concSnumbers[i] });
+                } else {
+                    concs.add(new Sint[] { (Sint) currentRangeStartingFrom });
+                    concs.add(new Sint[]{ (Sint) concSnumbers[i] });
+                }
+            }
+        }
+
+        List<Sint[]> syms = is.stream()
+                .filter(i -> i instanceof SymNumericExpressionSprimitive)
+                .map(i -> new Sint[] { i })
+                .collect(Collectors.toList());
+        concs.addAll(syms);
+
+        return concs;
+    }
 
     // Copy constructor, called to create a semantically equal version of ArraySolverRepresentation
     protected ArrayHistorySolverRepresentation(ArrayHistorySolverRepresentation toCopy) {
@@ -88,6 +171,10 @@ public class ArrayHistorySolverRepresentation {
             Sprimitive value,
             boolean arrayIsCompletelyInitialized,
             boolean defaultValueForUnknownsShouldBeEnforced) {
+        if (guard instanceof Sbool.ConcSbool && ((Sbool.ConcSbool) guard).isFalse()) {
+            // We do not need to add anything to the history of array accesses, as this access is not valid
+            return Sbool.ConcSbool.TRUE;
+        }
         return _select(
                 guard,
                 index,
@@ -105,17 +192,13 @@ public class ArrayHistorySolverRepresentation {
             Sprimitive value,
             boolean pushSelect,
             boolean defaultValueForUnknownsShouldBeEnforced) {
-        if (guard instanceof Sbool.ConcSbool && ((Sbool.ConcSbool) guard).isFalse()) {
-            // We do not need to add anything to the history of array accesses, as this access is not valid
-            return Sbool.ConcSbool.TRUE;
-        }
         Constraint indexEqualsToStoreIndexWithGuard;
         Constraint indexEqualsToStoreCase;
         Constraint resultForSelectOperations;
         // If we stored, we prioritize the stored index-value pair
         if (store != null) {
             assert beforeStore != null;
-            indexEqualsToStoreIndexWithGuard = And.newInstance(store.guard, Eq.newInstance(store.index, index));
+            indexEqualsToStoreIndexWithGuard = And.newInstance(store.guard, store.indexIsValid.apply(index));
             indexEqualsToStoreCase = elementsEqualConstraint(store.value, value);
             resultForSelectOperations = beforeStore._select(Sbool.ConcSbool.TRUE, index, value, false, false);
         } else {
@@ -127,7 +210,7 @@ public class ArrayHistorySolverRepresentation {
 
         // If it is not clear that the value must stem from the store operation, we check all previous selected values
         for (ArrayAccessSolverRepresentation s : selects) {
-            Constraint indexEqualsToSelectIndex = Eq.newInstance(s.index, index);
+            Constraint indexEqualsToSelectIndex = s.indexIsValid.apply(index);
             if (indexEqualsToSelectIndex instanceof Sbool.ConcSbool) {
                 // We can cut this short:
                 boolean doEqual = ((Sbool.ConcSbool) indexEqualsToSelectIndex).isTrue();
@@ -156,7 +239,7 @@ public class ArrayHistorySolverRepresentation {
             // If we have to enforce a default value, for instance 0 for int arrays or -1 for array-arrays, for unknown values, we
             // have to gather all guarded index-equals-constraints to use later
             Constraint indexEqualsToAnyPreviousIndexWithGuard =
-                    getFromHistory(h -> And.newInstance(h.guard, Eq.newInstance(index, h.index)))
+                    getFromHistory(h -> And.newInstance(h.guard, h.indexIsValid.apply(index)))
                             .stream()
                             .reduce(Sbool.ConcSbool.FALSE, Or::newInstance);
 
@@ -210,16 +293,6 @@ public class ArrayHistorySolverRepresentation {
         }
     }
 
-    public Set<Sint> getIndicesKnownToPossiblyBeContainedInArray(boolean arrayHasFixedLength) {
-        // In the case where the array has a fixed length, the array is fully initialized directly. And all indices
-        // are already given in the first layer of the array
-        if (arrayHasFixedLength) {
-            return getInitialConcreteAndStoredFromHistory(h -> h.index);
-        } else {
-            return getFromHistory(h -> h.index);
-        }
-    }
-
     private <T> Set<T> getFromHistory(Function<ArrayAccessSolverRepresentation, T> get) {
         Set<T> result = new HashSet<>();
         if (beforeStore != null) {
@@ -243,7 +316,7 @@ public class ArrayHistorySolverRepresentation {
         }
         for (ArrayAccessSolverRepresentation aasr : current.selects) {
             // Get the selects from the first representation which are not symbolic
-            if (aasr.index instanceof ConcSnumber && Sbool.ConcSbool.TRUE.equals(aasr.guard)) {
+            if (aasr.isConcrete && Sbool.ConcSbool.TRUE.equals(aasr.guard)) {
                 result.add(get.apply(aasr));
             }
         }
@@ -268,17 +341,27 @@ public class ArrayHistorySolverRepresentation {
         return BoolIte.newInstance(c, ifCase, elseCase);
     }
 
-    private static class ArrayAccessSolverRepresentation {
+    private static final class ArrayAccessSolverRepresentation {
         // The guard will typically be if the arrayId belonging to this ArrayHistorySolverRepresentation
         // is equal to some other symbolic ID
-        private final Constraint guard;
-        private final Sint index;
-        private final Sprimitive value;
+        final Constraint guard;
+        final Function<Sint, Constraint> indexIsValid;
+        final Sprimitive value;
+        final boolean isConcrete;
         ArrayAccessSolverRepresentation(Constraint guard, Sint index, Sprimitive value) {
             assert guard != null && index != null && value != null;
             this.guard = guard;
-            this.index = index;
+            this.indexIsValid = i -> Eq.newInstance(index, i);
             this.value = value;
+            this.isConcrete = index instanceof ConcSnumber;
+        }
+
+        ArrayAccessSolverRepresentation(Constraint guard, Function<Sint, Constraint> indexIsValid, Sprimitive value) {
+            assert guard != null && indexIsValid != null && value != null;
+            this.guard = guard;
+            this.indexIsValid = indexIsValid;
+            this.value = value;
+            this.isConcrete = true;
         }
     }
 }
