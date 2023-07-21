23db2159a205e5f50d410a9a5f253ab9e5da3446
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2023-07-21 13:32:15 +0200

    Added another Knapsack version. Is not yet performant, probably due to missing folding

--- src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/KnapsackExec.java
+++ src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/KnapsackExec.java
@@ -3,6 +3,7 @@ package de.wwu.mulib.transform_and_execute.examples_executor;
 import de.wwu.mulib.TestUtility;
 import de.wwu.mulib.search.trees.PathSolution;
 import de.wwu.mulib.transform_and_execute.examples.Knapsack2;
+import de.wwu.mulib.transform_and_execute.examples.KnapsackAlt;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -27,6 +28,27 @@ public class KnapsackExec {
                 ArrayList<Knapsack2.Item> returnValue = (ArrayList<Knapsack2.Item>) ps.getSolution().returnValue;
                 int accWeight = 0;
                 for (Knapsack2.Item it : returnValue) {
+                    accWeight += it.weight;
+                }
+                assertTrue(accWeight <= 4600);
+            }
+        }, "findKnapsack");
+    }
+
+    @Test
+    public void testKnapsackWithFreeArrays() {
+        TestUtility.getAllSolutions((mb) -> {
+            List<PathSolution> result = TestUtility.executeMulib(
+                    "findKnapsack",
+                    KnapsackAlt.class,
+                    mb,
+                    true
+            );
+            assertTrue(result.size() > 0);
+            for (PathSolution ps : result) {
+                ArrayList<KnapsackAlt.Item> returnValue = (ArrayList<KnapsackAlt.Item>) ps.getSolution().returnValue;
+                int accWeight = 0;
+                for (KnapsackAlt.Item it : returnValue) {
                     accWeight += it.weight;
                 }
                 assertTrue(accWeight <= 4600);
