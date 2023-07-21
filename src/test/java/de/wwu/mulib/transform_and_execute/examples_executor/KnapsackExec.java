e4c279297bf035c4c8e9f0557fcc4a673788beeb
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2023-07-21 10:10:32 +0200

    Removed setting logging level in KnapsackExec

--- src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/KnapsackExec.java
+++ src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/KnapsackExec.java
@@ -1,6 +1,5 @@
 package de.wwu.mulib.transform_and_execute.examples_executor;
 
-import de.wwu.mulib.Mulib;
 import de.wwu.mulib.TestUtility;
 import de.wwu.mulib.search.trees.PathSolution;
 import de.wwu.mulib.transform_and_execute.examples.Knapsack2;
@@ -8,7 +7,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,7 +15,6 @@ public class KnapsackExec {
 
     @Test
     public void testKnapsack() {
-        Mulib.setLogLevel(Level.FINE);
         TestUtility.getAllSolutions((mb) -> {
             List<PathSolution> result = TestUtility.executeMulib(
                     "findKnapsack",
