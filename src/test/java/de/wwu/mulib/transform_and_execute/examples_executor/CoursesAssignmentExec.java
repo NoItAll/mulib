be5517b872d1ed78a0be6b6d9a21ab11169cdee6
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2023-07-25 13:11:42 +0200

    Added test case for non-visible classes that have to be labeled

---
+++ src/test/java/de/wwu/mulib/transform_and_execute/examples_executor/CoursesAssignmentExec.java
@@ -0,0 +1,36 @@
+package de.wwu.mulib.transform_and_execute.examples_executor;
+
+import de.wwu.mulib.Mulib;
+import de.wwu.mulib.MulibConfig;
+import de.wwu.mulib.TestUtility;
+import de.wwu.mulib.search.trees.PathSolution;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CoursesAssignmentExec {
+
+    @Test
+    public void testScheduling() {
+        TestUtility.getAllSolutions(
+                this::runScheduling,
+                "testScheduling"
+        );
+    }
+
+    private List<PathSolution> runScheduling(MulibConfig.MulibConfigBuilder builder) {
+        List<PathSolution> result = Mulib.executeMulib("schedule", Courses.class, builder);
+        assertTrue(!result.isEmpty());
+        for (PathSolution ps : result) {
+            ArrayList<Courses.Assignment> assignments = (ArrayList<Courses.Assignment>) ps.getSolution().returnValue;
+            for (Courses.Assignment a : assignments) {
+                assertNotNull(a);
+            }
+        }
+        return result;
+    }
+}
